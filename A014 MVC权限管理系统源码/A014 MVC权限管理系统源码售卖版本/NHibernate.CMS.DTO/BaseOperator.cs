90e8f8efeae0f669f89161cef04352c657500fe8
rex <2363613998@qq.com>
2016-10-22 16:40:31 +0800

    初始化项目

---
+++ A014 MVC权限管理系统源码/A014 MVC权限管理系统源码售卖版本/NHibernate.CMS.DTO/BaseOperator.cs
@@ -0,0 +1,31 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NHibernate.CMS.DTO
+{
+    /// <summary>
+    /// NHibernate调用基类--用法1
+    /// </summary>
+    public class BaseOperator
+    {
+        private ISession m_Session;
+        public ISession Session
+        {
+            get { return m_Session; }
+        }
+
+        private ISessionFactory m_SessionFactory;
+        public BaseOperator()
+        {
+
+            var config = new Configuration().Configure("E:/郝光明工作/工作/代码审核/12月份工作/12.3/第九个mvc/最新/NHibernate.CMS.MVC/bin/Config/hibernate.cfg.xml");
+            m_SessionFactory = config.BuildSessionFactory();
+            m_Session = m_SessionFactory.OpenSession();
+        }
+    }
+
+}
